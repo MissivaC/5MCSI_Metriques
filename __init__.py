df77d413e5e1bff10df74a75a4190ea8ad5ccc6f
MissivaC <159757658+MissivaC@users.noreply.github.com>
2024-02-12 10:25:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,5 +34,9 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
   
+@app.route("/Contact/")
+def monContact():
+    return render_template("Contact.html")
+  
 if __name__ == "__main__":
   app.run(debug=True)
